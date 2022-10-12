010c88a46f818f2a0f9b9c35005436aa1dd09460
TomerAsraf <tomerasraf2@gmail.com>
2022-10-12 21:39:12 +0300

    Editor Update 2
    + division notes
    + spray mode
    + zoom Control
    + Count Bar

--- Assets/Scripts/Camera/EditorCamera.cs
+++ Assets/Scripts/Camera/EditorCamera.cs
@@ -4,11 +4,29 @@ using UnityEngine;
 
 public class EditorCamera : MonoBehaviour
 {
-    public float dragSpeed = 2;
+    [Header("Camera Control")]
+    [SerializeField] float dragSpeed = 2;
+    [SerializeField] float minFov = 15f;
+    [SerializeField] float maxFov = 90f;
+    [SerializeField] float sensitivity = 10f;
+
     private Vector3 dragOrigin;
 
 
     void Update()
+    {
+        ZoomControl();
+        DragCamera();
+    }
+
+    void ZoomControl() {
+        float fov = Camera.main.fieldOfView;
+        fov += Input.GetAxis("Mouse ScrollWheel") * -sensitivity;
+        fov = Mathf.Clamp(fov, minFov, maxFov);
+        Camera.main.fieldOfView = fov;
+    } 
+
+     void DragCamera()
     {
         if (Input.GetMouseButtonDown(0))
         {
@@ -19,7 +37,7 @@ public class EditorCamera : MonoBehaviour
         if (!Input.GetMouseButton(0)) return;
 
         Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragOrigin);
-  
+
         Vector3 move = new Vector3(pos.x * dragSpeed, 0, pos.y * dragSpeed);
 
         transform.Translate(move, Space.World);
--- Assets/Scripts/Level Editor/EditorManager.cs
+++ Assets/Scripts/Level Editor/EditorManager.cs
@@ -1,7 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine.UI;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EditorManager : MonoBehaviour
 {
@@ -15,22 +14,30 @@ public class EditorManager : MonoBehaviour
     [SerializeField] GameObject BlueShape;
     [SerializeField] GameObject RedShape;
 
+    [Header("Transforms")]
+    [SerializeField] Transform level;
+
     [Header("Buttons")]
     [SerializeField] Button blueButton;
     [SerializeField] Button redButton;
+    [SerializeField] Button sprayModeButton;
+    [SerializeField] Button updateBarsButton;
 
     [Header("Vars")]
     [SerializeField] float barLengthByMeter;
     [SerializeField] int bars;
+    [SerializeField] int noteDiv;
 
     [Header("Utils")]
     [SerializeField] GameObject barPrefab;
+    [SerializeField] GameObject divisionBarPrefab;
+    [SerializeField] GameObject barNumTextPrefab;
     [SerializeField] Transform startingBarPosition;
 
     // ture = blue false = red
     bool SpawnBlueOrRed;
-    bool isMouseOnSlot;
-    bool isMouseOnShape;
+    bool sprayModeIsOn = true;
+    float CurrentDivisionLength;
 
     Vector3 lineOriginPosition;
 
@@ -47,10 +54,31 @@ public class EditorManager : MonoBehaviour
         {
             SpawnBlueOrRed = false;
         });
+
+        sprayModeButton.onClick.AddListener(() =>
+        {
+            sprayModeIsOn = !sprayModeIsOn;
+
+            if (sprayModeIsOn)
+            {
+                sprayModeButton.GetComponentInChildren<TextMeshProUGUI>().text = "Spray mode ON";
+            }
+            else
+            {
+                sprayModeButton.GetComponentInChildren<TextMeshProUGUI>().text = "Spray mode OFF";
+            }
+        });
+
+        updateBarsButton.onClick.AddListener(() =>
+        {
+
+        });
+
     }
 
     private void Start()
     {
+        CurrentDivisionLength = barLengthByMeter / 4;
         InitializeBars();
     }
 
@@ -59,19 +87,37 @@ public class EditorManager : MonoBehaviour
         EditorLogic();
     }
 
-    void InitializeBars() {
+    void InitializeBars()
+    {
 
         lineOriginPosition = startingBarPosition.position;
-       
-        for (int i = 0; i < bars; i++)
+
+        for (int i = 1; i < bars; i++)
         {
             float nextLineZPos = lineOriginPosition.z + (barLengthByMeter * i);
-            Instantiate(barPrefab, new Vector3(0,4.71f, nextLineZPos),Quaternion.Euler(0,0,90));
+            GameObject bar = Instantiate(barPrefab, new Vector3(0, 4.71f, nextLineZPos), Quaternion.Euler(0, 0, 90));
+            GameObject BarIndex = Instantiate(barNumTextPrefab, bar.transform.position - Vector3.right * 15, Quaternion.Euler(90, 0, 0));
+            BarIndex.GetComponent<TextMeshPro>().text = i.ToString();
+
+            float currentLine = bar.transform.position.z + CurrentDivisionLength;
+
+            for (int y = 0; y < 3; y++)
+            {
+                GameObject divisionBar = Instantiate(divisionBarPrefab, new Vector3(0, 4.71f, currentLine), Quaternion.Euler(0, 0, 90));
+                GameObject divisionIndex = Instantiate(barNumTextPrefab, divisionBar.transform.position - Vector3.right * 13, Quaternion.Euler(90,0,0));
+                divisionIndex.GetComponent<TextMeshPro>().text = $"{i}.{y + 2}";
+                currentLine += CurrentDivisionLength;
+
+            }
         }
-        
     }
 
-    private void EditorLogic()
+    void UpdateBars()
+    {
+
+    }
+
+    void EditorLogic()
     {
         // Geting the position of the finger on the screen.
         Vector3 mousePos = Input.mousePosition;
@@ -84,22 +130,39 @@ public class EditorManager : MonoBehaviour
         RaycastHit hitSlot;
         RaycastHit hitShape;
 
-        // Shooting a ray from the camera to the desire player.
+        // Shooting a ray from the camera to the desired object.
         if (Physics.Raycast(touchPosN, touchPosF - touchPosN, out hitSlot, 100, slotLayer))
         {
 
             if (transparentClone == null)
                 transparentClone = Instantiate(SpawnBlueOrRed ? transparentBlueShape : transparentRedShape, hitSlot.transform.position, Quaternion.identity);
 
-            if (Input.GetMouseButtonDown(0) && !Physics.Raycast(touchPosN, touchPosF - touchPosN, out hitShape, 100, shapeLayer))
+            if (sprayModeIsOn)
             {
-                Instantiate(SpawnBlueOrRed ? BlueShape : RedShape, hitSlot.transform.position, Quaternion.identity);
+
+                if (Input.GetMouseButton(0) && !Physics.Raycast(touchPosN, touchPosF - touchPosN, out hitShape, 100, shapeLayer))
+                {
+                    Instantiate(SpawnBlueOrRed ? BlueShape : RedShape, hitSlot.transform.position, Quaternion.identity, level);
+                }
+
+                if (Input.GetMouseButton(1) && Physics.Raycast(touchPosN, touchPosF - touchPosN, out hitShape, 100, shapeLayer))
+                {
+                    Destroy(hitShape.transform.gameObject);
+                }
+            }
+            else
+            {
+                if (Input.GetMouseButtonDown(0) && !Physics.Raycast(touchPosN, touchPosF - touchPosN, out hitShape, 100, shapeLayer))
+                {
+                    Instantiate(SpawnBlueOrRed ? BlueShape : RedShape, hitSlot.transform.position, Quaternion.identity, level);
+                }
+
+                if (Input.GetMouseButtonDown(1) && Physics.Raycast(touchPosN, touchPosF - touchPosN, out hitShape, 100, shapeLayer))
+                {
+                    Destroy(hitShape.transform.gameObject);
+                }
             }
 
-            if (Input.GetMouseButtonDown(1) && Physics.Raycast(touchPosN, touchPosF - touchPosN, out hitShape, 100, shapeLayer))
-            {
-                Destroy(hitShape.transform.gameObject);
-            }
         }
         else
         {
